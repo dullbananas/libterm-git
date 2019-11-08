301239dddf981caa8f698fd7a83330d90eef432b
Dull Bananas <dull.bananas0@gmail.com>
2019-11-08 15:11:28 -0700

    Add error message for click import; improve code structure of git status sections

--- git.py
+++ git.py
@@ -1,4 +1,7 @@
-import click
+try:
+	import click
+except ImportError:
+	print('Error: click is not installed. Use "python -m pip install click" to install it.')
 import sys
 import os
 from dulwich import porcelain as gitp
@@ -30,6 +33,18 @@ def cli(ctx, version, change_dir):
 		sys.exit()
 
 
+def st_section(title, help, items, color):
+	if len(items) == 0:
+		return
+	click.echo(
+		f'{title}:\n'
+		f'  ({help})\n'
+		f'{color}'
+	)
+	for item in items:
+		click.echo(f'\t{item}')
+	click.echo(f'{ANSI["default"]}\n')
+
 @cli.command()
 @click.pass_context
 def status(ctx):
@@ -37,16 +52,23 @@ def status(ctx):
 	click.echo(repr(st))
 	#TODO: add "On branch [branchname]"
 	if True: # <-- will be for --long
-		click.echo('Changes to be comitted:')
-		click.echo('  (use "git reset HEAD <file>..." to unstage)\n'+ANSI['green'])
-		for path in st.staged['add']:
-			click.echo(f'\tnew file:   {path.decode("utf-8")}')
+		# Changes to be committed (staged)
+		staged = []
+		for newfile in st.staged['add']:
+			staged.append(f'new file:   {newfile.decode("utf-8")}')
+		for modified in st.staged['modify']:
+			staged.append(f'modified:   {modified.decode("utf-8")}')
+		st_section('Changes to be committed', 'use "git reset HEAD <file>..." to unstage', staged, ANSI['green'])
+		#click.echo('Changes to be comitted:')
+		#click.echo('  (use "git reset HEAD <file>..." to unstage)\n'+ANSI['green'])
+		#for path in st.staged['add']:
+		#	click.echo(f'\tnew file:   {path.decode("utf-8")}')
 		
-		click.echo(ANSI['default']+'\nUntracked files:')
-		click.echo('  (use "git add <file>..." to include in what will be committed)\n'+ANSI['red'])
-		for path in st.untracked:
-			click.echo(f'\t{path}')
-		click.echo()
+		#click.echo(ANSI['default']+'\nUntracked files:')
+		#click.echo('  (use "git add <file>..." to include in what will be committed)\n'+ANSI['red'])
+		#for path in st.untracked:
+		#	click.echo(f'\t{path}')
+		#click.echo()
 
 
 if __name__ == '__main__':
