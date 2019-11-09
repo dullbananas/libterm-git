cbd87fbee21839e55c2df55e0203c9b21b06ac09
Dull Bananas <dull.bananas0@gmail.com>
2019-11-09 10:36:46 -0700

    Add aditional commands

--- git.py
+++ git.py
@@ -4,7 +4,10 @@ except ImportError:
 	print('Error: click is not installed. Use "python -m pip install click" to install it.')
 import sys
 import os
+import itertools
+import re
 from dulwich import porcelain as gitp
+from dulwich.repo import Repo
 
 __version__ = '0.0'
 
@@ -15,32 +18,68 @@ ANSI = {
 	'default': '\033[39m',
 }
 
+def active_branch(repo):
+	(_, ref), _ = repo.refs.follow(b'HEAD')
+	match = re.search(r'/([^/]+)$', ref.decode('utf-8'))
+	return match[1]
+
 
 @click.group(invoke_without_command=True)
 @click.option('--version', is_flag=True, help='Show version of libterm-git and exit')
 @click.option('--change-dir', '-C', default='', help='Change to directory before doing anything')
 @click.pass_context
 def cli(ctx, version, change_dir):
-	#click.echo(ANSI['default'], nl=False)
-	ctx.ensure_object(dict)
-	ctx.obj['repo'] = '.'
+	if version:
+		click.echo(f'libterm-git version {__version__} by Dull Bananas (https://dull.pythonanywhere.com)')
+		sys.exit()
 	
 	if change_dir != '':
 		os.chdir(change_dir)
 	
-	if version:
-		click.echo(f'libterm-git version {__version__} by Dull Bananas (https://dull.pythonanywhere.com)')
-		sys.exit()
+	ctx.ensure_object(dict)
+	ctx.obj['repo'] = '.'
+	ctx.obj['repo_obj'] = Repo('.')
+
+
+@cli.command()
+@click.argument('paths', nargs=-1)
+@click.pass_context
+def add(ctx, paths):
+	if '.' in paths:
+		click.echo('Warning: adding "." does not work yet')
+	result = gitp.add(ctx.obj['repo'], list(paths))
+	click.echo(result)
+
+
+@cli.command()
+@click.argument('source')
+@click.pass_context
+def clone(ctx, source):
+	gitp.clone(
+		source=source,
+	)
+
+
+@cli.command()
+@click.option('--message', '-m', multiple=True)
+@click.pass_context
+def commit(ctx, message):
+	gitp.commit(ctx.obj['repo'], '\n\n'.join(message))
+
+
+@cli.command()
+#@click.argument('remote')
+#@click.argument('branch')
+@click.pass_context
+def push(ctx):
+	gitp.push(ctx.obj['repo'], 'refs/remotes/origin/master', 'master')
 
 
 def st_section(title, help, items, color):
-	if len(items) == 0:
-		return
-	click.echo(
-		f'{title}:\n'
-		f'  ({help})\n'
-		f'{color}'
-	)
+	click.echo(title+':')
+	for h in help:
+		click.echo(f'  ({h})')
+	click.echo(color)
 	for item in items:
 		click.echo(f'\t{item}')
 	click.echo(f'{ANSI["default"]}\n')
@@ -48,27 +87,40 @@ def st_section(title, help, items, color):
 @cli.command()
 @click.pass_context
 def status(ctx):
+	# Get status
 	st = gitp.status(ctx.obj['repo'])
-	click.echo(repr(st))
-	#TODO: add "On branch [branchname]"
+	added_new = (name.decode('utf-8') for name in st.staged['add'])
+	added_modified = (name.decode('utf-8') for name in st.staged['modify'])
+	added_deleted = (name.decode('utf-8') for name in st.staged['delete'])
+	unstaged = (name.decode('utf-8') for name in st.unstaged)
+	untracked = st.untracked
+	#click.echo(repr(st))
+	
 	if True: # <-- will be for --long
-		# Changes to be committed (staged)
-		staged = []
-		for newfile in st.staged['add']:
-			staged.append(f'new file:   {newfile.decode("utf-8")}')
-		for modified in st.staged['modify']:
-			staged.append(f'modified:   {modified.decode("utf-8")}')
-		st_section('Changes to be committed', 'use "git reset HEAD <file>..." to unstage', staged, ANSI['green'])
-		#click.echo('Changes to be comitted:')
-		#click.echo('  (use "git reset HEAD <file>..." to unstage)\n'+ANSI['green'])
-		#for path in st.staged['add']:
-		#	click.echo(f'\tnew file:   {path.decode("utf-8")}')
-		
-		#click.echo(ANSI['default']+'\nUntracked files:')
-		#click.echo('  (use "git add <file>..." to include in what will be committed)\n'+ANSI['red'])
-		#for path in st.untracked:
-		#	click.echo(f'\t{path}')
-		#click.echo()
+		click.echo('On branch '+active_branch(ctx.obj['repo_obj']))
+		st_section(
+			'Changes to be committed',
+			('use "git reset HEAD <file>..." to unstage',),
+			itertools.chain(
+				('deleted:    '+i for i in added_deleted),
+				('modified:   '+i for i in added_modified),
+				('new file:   '+i for i in added_new),
+			),
+			ANSI['green'],
+		)
+		st_section(
+			'Changes not staged for commit',
+			('use "git add <file>..." to update what will be committed',
+			'use "git checkout -- <file>..." to discard changes in working directory',),
+			unstaged,
+			ANSI['red'],
+		)
+		st_section(
+			'Untracked files',
+			('use "git add <file>..." to include in what will be committed',),
+			untracked,
+			ANSI['red'],
+		)
 
 
 if __name__ == '__main__':
